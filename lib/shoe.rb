a39eaea56507ed2504f5ccd584e1c70b87770b6e
Daryl Rogony <darylrogony@gmail.com>
2022-06-10 15:01:15 +0300

    Completed: working on different aspects of the object orientation basics of ruby

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,21 @@
 # Make your shoe class here!
+
+class Shoe
+  attr_reader :brand
+  attr_accessor :color, :size, :material, :condition
+
+  def initialize(brand)
+    @brand = brand
+  end
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+end
+
+Shoes= Shoe.new("Adidas")
+Shoes.brand
+Shoes.color
+Shoes.size
+Shoes.material
+Shoes.condition
